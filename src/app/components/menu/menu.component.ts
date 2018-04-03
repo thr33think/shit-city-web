dd06be6e8a338721ab6dfed007dd6be282693831
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2018-04-03 16:35:27 +0200

    Improve modal and buttons

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,11 +9,20 @@ import { trigger, transition, animate, style } from '@angular/animations';
   animations: [
     trigger('slideInOut', [
       transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('500ms ease-in', style({ transform: 'translateX(0%)' }))
+        style({ transform: 'translateY(+100%)' }),
+        animate('200ms ease-in', style({ transform: 'translateY(0%)' }))
       ]),
       transition(':leave', [
-        animate('500ms ease-in', style({ transform: 'translateX(-100%)' }))
+        animate('200ms ease-in', style({ transform: 'translateY(+100%)' }))
+      ])
+    ]),
+    trigger('opacityFade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0 }))
       ])
     ])
   ]
