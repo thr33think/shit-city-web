40eaadc58ad70921a43f11eb8fe955943c6a74ad
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2018-04-17 14:35:50 +0200

    Add typing and delete unused code

--- src/app/services/turd-api.service.ts
+++ src/app/services/turd-api.service.ts
@@ -1,55 +1,28 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Marker } from '../interfaces/marker';
+import { Marker, Markers } from '../interfaces/marker';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class TurdApiService {
 
-  markers: Marker[] = [
-    {
-      lat: 51.673858,
-      long: 7.815982,
-      id: 'A',
-      image_base64: '/assets/images/poo.png',
-      timestamp: '0',
-      visible: true
-    },
-    {
-      lat: 51.373858,
-      long: 7.215982,
-      id: 'B',
-      image_base64: '/assets/images/poo.png',
-      timestamp: '0',
-      visible: true
-    },
-    {
-      lat: 51.723858,
-      long: 7.895982,
-      id: 'C',
-      image_base64: '/assets/images/poo.png',
-      timestamp: '0',
-      visible: true
-    }
-  ];
-
   constructor(private http: HttpClient) { }
 
-  async getTurds(): Promise<any> {
-    const markers = await this.http.get(`${environment.apiUrl}/turds`, {
+  async getTurds(): Promise<Markers[]> {
+    const markers = await this.http.get<Markers[]>(`${environment.apiUrl}/turds`, {
       headers: new HttpHeaders().set('Authorization', 'Basic YXBwOmpCSDNKSnZnN0d1VTFIQ00zZkxuSnQxQXE=')
     }).toPromise();
     return markers;
   }
 
-  async getTurd(id): Promise<any>  {
-    const marker = await this.http.get(`${environment.apiUrl}/turds/${id}`, {
+  async getTurd(id: string): Promise<Marker>  {
+    const marker = await this.http.get<Marker>(`${environment.apiUrl}/turds/${id}`, {
       headers: new HttpHeaders().set('Authorization', 'Basic YXBwOmpCSDNKSnZnN0d1VTFIQ00zZkxuSnQxQXE=')
     }).toPromise();
     return marker;
   }
 
-  async postTurd(bodyData): Promise<any> {
+  async postTurd(bodyData: Marker): Promise<any> {
     const upload = await this.http.post(`${environment.apiUrl}/turds`, bodyData, {
       headers: new HttpHeaders().set('Authorization', 'Basic YXBwOmpCSDNKSnZnN0d1VTFIQ00zZkxuSnQxQXE=')
     }).toPromise();
