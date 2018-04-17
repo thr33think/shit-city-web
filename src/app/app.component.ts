036bdc243d83f1b6c43f9bbb7f7006042df9fa8d
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2018-04-17 16:07:45 +0200

    Add version and git output into console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { versions } from './../environments/versions';
 import { MapComponent } from './components/map/map.component';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import * as data from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,32 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
+  private styleGreen = 'color:white; background:#61ba96';
+  private styleRed = 'color:white; background:#d36a6a';
+  private styleOrange = 'color:white; background:#F29526';
+  private styleBlueish = 'color:white; background:#99C7FF';
+
+
   @ViewChild(MapComponent) map;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(
+      '%c Threethink %c v' +
+      (<any>data).version +
+      ' %c Revision: ' +
+      (<any>versions).revision +
+      ' %c Branch: ' +
+      (<any>versions).branch +
+      ' ',
+
+      this.styleGreen,
+      this.styleRed,
+      this.styleOrange,
+      this.styleBlueish
+    );
+  }
 
   centerMap() {
     this.map.centerMap();
